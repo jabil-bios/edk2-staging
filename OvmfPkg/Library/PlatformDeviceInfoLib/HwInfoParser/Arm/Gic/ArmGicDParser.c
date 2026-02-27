tianocore/edk2

OvmfPkg: PlatformDeviceInfoLib - add Arm GIC parser
Add functionality to parse the GIC objects from the
FDT and populate the PLATFORM_DEVICE_INFO structure.

This patch adds functionality to parse the following
GIC components and populate their base address and
range in the PLATFORM_DEVICE_INFO structure.
 - GICC
 - GICD
 - GICR

Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
Cc: Leif Lindholm <quic_llindhol@quicinc.com>
Cc: Gerd Hoffmann <kraxel@redhat.com>
Signed-off-by: Sami Mujawar <sami.mujawar@arm.com>

OvmfPkg/Library/PlatformDeviceInfoLib/HwInfoParser/Arm/Gic/ArmGicDParser.c
@@ -1,0 +1,169 @@
+/** @file
+  Arm Gic Distributor Parser.
+
+  Copyright (c) 2021 - 2024, Arm Limited. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+  @par Reference(s):
+  - linux/Documentation/devicetree/bindings/interrupt-controller/arm,gic.yaml
+  - linux/Documentation/devicetree/bindings/interrupt-controller/arm,gic-v3.yaml
+**/
+
+#include "FdtUtility.h"
+#include "FdtInfoParser.h"
+#include "ArmGicDispatcher.h"
+#include "ArmGicDParser.h"
+
+/** Parse a Gic compatible interrupt-controller node,
+    extracting GicD information.
+
+  This parser is valid for Gic v2 and v3.
+
+  @param [in]  FdtParserHandle  A handle to the parser instance.
+  @param [in]  GicIntcNode      Offset of a Gic compatible
+                                interrupt-controller node.
+
+  @retval EFI_SUCCESS             The function completed successfully.
+  @retval EFI_ABORTED             An error occurred.
+  @retval EFI_INVALID_PARAMETER   Invalid parameter.
+**/
+STATIC
+EFI_STATUS
+EFIAPI
+GicDIntcNodeParser (
+  IN  CONST FDT_HW_INFO_PARSER_HANDLE  FdtParserHandle,
+  IN  INT32                            GicIntcNode
+  )
+{
+  EFI_STATUS   Status;
+  INT32        AddressCells;
+  INT32        SizeCells;
+  CONST UINT8  *Data;
+  INT32        DataSize;
+  UINT32       RegSize;
+  UINT64       PhysicalBaseAddress;
+  UINT64       PhysicalBaseAddressRange;
+  VOID         *Fdt;
+  CONST UINT8  *GicDValue;
+  CONST UINT8  *GicDRange;
+
+  if (FdtParserHandle == NULL) {
+    ASSERT (FALSE);
+    return EFI_INVALID_PARAMETER;
+  }
+
+  Fdt = FdtParserHandle->Fdt;
+
+  Status = FdtGetParentAddressInfo (
+             Fdt,
+             GicIntcNode,
+             &AddressCells,
+             &SizeCells
+             );
+  if (EFI_ERROR (Status)) {
+    ASSERT (FALSE);
+    return Status;
+  }
+
+  // Don't support more than 64 bits and less than 32 bits addresses.
+  if ((AddressCells < 1) ||
+      (AddressCells > 2) ||
+      (SizeCells < 1) ||
+      (SizeCells > 2))
+  {
+    ASSERT (FALSE);
+    return EFI_ABORTED;
+  }
+
+  RegSize = (AddressCells + SizeCells) * sizeof (UINT32);
+
+  Data = fdt_getprop (Fdt, GicIntcNode, "reg", &DataSize);
+  if ((Data == NULL) ||
+      (DataSize < 0) ||
+      ((DataSize % RegSize) != 0))
+  {
+    // If error or wrong size.
+    ASSERT (FALSE);
+    return EFI_ABORTED;
+  }
+
+  GicDValue = Data + (sizeof (UINT32) *
+                      GET_DT_REG_ADDRESS_OFFSET (0, AddressCells, SizeCells));
+  GicDRange = Data + sizeof (UINT32) * AddressCells;
+
+  if (AddressCells == 2) {
+    PhysicalBaseAddress      = FdtReadUnaligned64 ((UINT64 *)GicDValue);
+    PhysicalBaseAddressRange = FdtReadUnaligned64 ((UINT64 *)GicDRange);
+  } else {
+    PhysicalBaseAddress      = FdtReadUnaligned32 ((UINT32 *)GicDValue);
+    PhysicalBaseAddressRange = FdtReadUnaligned32 ((UINT32 *)GicDRange);
+  }
+
+  if (FdtParserHandle->HwAddressInfo != NULL) {
+    Status = FdtParserHandle->HwAddressInfo (
+                                FdtParserHandle->Context,
+                                "GICD",
+                                PhysicalBaseAddress,
+                                PhysicalBaseAddressRange
+                                );
+    ASSERT_EFI_ERROR (Status);
+  }
+
+  return Status;
+}
+
+/** GICD information parser function.
+
+  @param [in]  FdtParserHandle  A handle to the parser instance.
+  @param [in]  FdtBranch        When searching for DT node name, restrict
+                                the search to this Device Tree branch.
+  @param [in]  GicVersion       The version of the GIC.
+
+  @retval EFI_SUCCESS             The function completed successfully.
+  @retval EFI_ABORTED             An error occurred.
+  @retval EFI_INVALID_PARAMETER   Invalid parameter.
+  @retval EFI_NOT_FOUND           Not found.
+  @retval EFI_UNSUPPORTED         Unsupported.
+**/
+EFI_STATUS
+EFIAPI
+ArmGicDInfoParser (
+  IN  CONST FDT_HW_INFO_PARSER_HANDLE  FdtParserHandle,
+  IN        INT32                      FdtBranch,
+  IN  CONST UINT32                     GicVersion
+  )
+{
+  EFI_STATUS  Status;
+  VOID        *Fdt;
+
+  if (FdtParserHandle == NULL) {
+    ASSERT (FALSE);
+    return EFI_INVALID_PARAMETER;
+  }
+
+  Fdt = FdtParserHandle->Fdt;
+
+  if (!FdtNodeHasProperty (Fdt, FdtBranch, "interrupt-controller")) {
+    ASSERT (FALSE);
+    return EFI_INVALID_PARAMETER;
+  }
+
+  // Parse the interrupt-controller depending on its Gic version.
+  switch (GicVersion) {
+    case 2:
+    case 3:
+    {
+      // Set the Gic version, then parse the GicD information.
+      Status = GicDIntcNodeParser (FdtParserHandle, FdtBranch);
+      break;
+    }
+    default:
+    {
+      // Unsupported Gic version.
+      ASSERT (FALSE);
+      return EFI_UNSUPPORTED;
+    }
+  }
+
+  return Status;
+}

OvmfPkg/Library/PlatformDeviceInfoLib/HwInfoParser/Arm/Gic/ArmGicDispatcher.c
@@ -1,0 +1,189 @@
+/** @file
+  Arm Gic dispatcher.
+
+  Copyright (c) 2021 - 2024, Arm Limited. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+  @par Reference(s):
+  - linux/Documentation/devicetree/bindings/interrupt-controller/arm,gic.yaml
+  - linux/Documentation/devicetree/bindings/interrupt-controller/arm,gic-v3.yaml
+**/
+
+#include "FdtUtility.h"
+#include "FdtInfoParser.h"
+#include "ArmGicCParser.h"
+#include "ArmGicDispatcher.h"
+#include "ArmGicDParser.h"
+#include "ArmGicRParser.h"
+
+/** List of "compatible" property values for GicV2 interrupt nodes.
+
+  Any other "compatible" value is not supported by this module.
+*/
+STATIC CONST COMPATIBILITY_STR  GicV2CompatibleStr[] = {
+  { "arm,cortex-a15-gic" }
+};
+
+/** COMPATIBILITY_INFO structure for the GICv2.
+*/
+CONST COMPATIBILITY_INFO  GicV2CompatibleInfo = {
+  ARRAY_SIZE (GicV2CompatibleStr),
+  GicV2CompatibleStr
+};
+
+/** List of "compatible" property values for GicV3 interrupt nodes.
+
+  Any other "compatible" value is not supported by this module.
+*/
+STATIC CONST COMPATIBILITY_STR  GicV3CompatibleStr[] = {
+  { "arm,gic-v3" }
+};
+
+/** COMPATIBILITY_INFO structure for the GICv3.
+*/
+CONST COMPATIBILITY_INFO  GicV3CompatibleInfo = {
+  ARRAY_SIZE (GicV3CompatibleStr),
+  GicV3CompatibleStr
+};
+
+/** Get the Gic version of an interrupt-controller node.
+
+  @param [in]  Fdt          Pointer to a Flattened Device Tree (Fdt).
+  @param [in]  IntcNode     Interrupt-controller node.
+  @param [out] GicVersion   If success, contains the Gic version of the
+                            interrupt-controller node.
+
+  @retval EFI_SUCCESS             The function completed successfully.
+  @retval EFI_INVALID_PARAMETER   Invalid parameter.
+  @retval EFI_UNSUPPORTED         Unsupported.
+**/
+STATIC
+EFI_STATUS
+EFIAPI
+GetGicVersion (
+  IN  CONST VOID    *Fdt,
+  IN        INT32   IntcNode,
+  OUT       UINT32  *GicVersion
+  )
+{
+  if ((Fdt == NULL) ||
+      (GicVersion == NULL))
+  {
+    ASSERT (FALSE);
+    return EFI_INVALID_PARAMETER;
+  }
+
+  if (FdtNodeIsCompatible (Fdt, IntcNode, &GicV2CompatibleInfo)) {
+    *GicVersion = 2;
+  } else if (FdtNodeIsCompatible (Fdt, IntcNode, &GicV3CompatibleInfo)) {
+    *GicVersion = 3;
+  } else {
+    // Unsupported Gic version.
+    ASSERT (FALSE);
+    return EFI_UNSUPPORTED;
+  }
+
+  return EFI_SUCCESS;
+}
+
+/** Gic dispatcher.
+
+  This disptacher parses the Device tree for the following infromation:
+   - GICC
+   - GICD
+   - GICR
+
+  @param [in]  FdtParserHandle A handle to the parser instance.
+  @param [in]  FdtBranch       When searching for DT node name, restrict
+                               the search to this Device Tree branch.
+
+  @retval EFI_SUCCESS             The function completed successfully.
+  @retval EFI_ABORTED             An error occurred.
+  @retval EFI_INVALID_PARAMETER   Invalid parameter.
+  @retval EFI_NOT_FOUND           Not found.
+  @retval EFI_UNSUPPORTED         Unsupported.
+**/
+EFI_STATUS
+EFIAPI
+ArmGicDispatcher (
+  IN  CONST FDT_HW_INFO_PARSER_HANDLE  FdtParserHandle,
+  IN        INT32                      FdtBranch
+  )
+{
+  EFI_STATUS  Status;
+  INT32       CpusNode;
+  INT32       IntcNode;
+  UINT32      GicVersion;
+  VOID        *Fdt;
+
+  if (FdtParserHandle == NULL) {
+    ASSERT (FALSE);
+    return EFI_INVALID_PARAMETER;
+  }
+
+  Fdt = FdtParserHandle->Fdt;
+
+  // The "cpus" node resides at the root of the DT. Fetch it.
+  CpusNode = fdt_path_offset (Fdt, "/cpus");
+  if (CpusNode < 0) {
+    return EFI_NOT_FOUND;
+  }
+
+  // Get the interrupt-controller node associated to the "cpus" node.
+  Status = FdtGetIntcParentNode (Fdt, CpusNode, &IntcNode);
+  if (EFI_ERROR (Status)) {
+    ASSERT (FALSE);
+    if (Status == EFI_NOT_FOUND) {
+      // Should have found the node.
+      Status = EFI_ABORTED;
+    }
+
+    return Status;
+  }
+
+  Status = GetGicVersion (Fdt, IntcNode, &GicVersion);
+  if (EFI_ERROR (Status)) {
+    ASSERT (FALSE);
+    return Status;
+  }
+
+  // Parse the GicC information.
+  Status = ArmGicCInfoParser (FdtParserHandle, IntcNode, GicVersion);
+  if (EFI_ERROR (Status)) {
+    // Don't try to parse GicD and GicMsiFrame information
+    // if no GicC information is found. Return.
+    ASSERT (Status == EFI_NOT_FOUND);
+    return Status;
+  }
+
+  // Parse the GicD information of the "cpus" interrupt-controller node.
+  Status = ArmGicDInfoParser (FdtParserHandle, IntcNode, GicVersion);
+  if (EFI_ERROR (Status)) {
+    // EFI_NOT_FOUND is not tolerated at this point.
+    ASSERT (FALSE);
+    return Status;
+  }
+
+  switch (GicVersion) {
+    case 4:
+    case 3:
+    {
+      // Parse the GicR information of the interrupt-controller node.
+      Status = ArmGicRInfoParser (FdtParserHandle, IntcNode, GicVersion);
+      if (EFI_ERROR (Status)) {
+        // EFI_NOT_FOUND is not tolerated at this point.
+        ASSERT (FALSE);
+        return Status;
+      }
+
+      break;
+    }
+    default:
+    {
+      ASSERT (FALSE);
+      return EFI_UNSUPPORTED;
+    }
+  }
+
+  return EFI_SUCCESS;
+}

jabil-bios/edk2-staging

Cc: Leif Lindholm <leif.lindholm@oss.qualcomm.com>

OvmfPkg/Library/PlatformDeviceInfoLib/HwInfoParser/Arm/Gic/ArmGicDParser.c
@@ -1,0 +1,169 @@
+/** @file
+  Arm Gic Distributor Parser.
+
+  Copyright (c) 2021 - 2024, Arm Limited. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+  @par Reference(s):
+  - linux/Documentation/devicetree/bindings/interrupt-controller/arm,gic.yaml
+  - linux/Documentation/devicetree/bindings/interrupt-controller/arm,gic-v3.yaml
+**/
+
+#include "FdtUtility.h"
+#include "FdtInfoParser.h"
+#include "ArmGicDispatcher.h"
+#include "ArmGicDParser.h"
+
+/** Parse a Gic compatible interrupt-controller node,
+    extracting GicD information.
+
+  This parser is valid for Gic v2 and v3.
+
+  @param [in]  FdtParserHandle  A handle to the parser instance.
+  @param [in]  GicIntcNode      Offset of a Gic compatible
+                                interrupt-controller node.
+
+  @retval EFI_SUCCESS             The function completed successfully.
+  @retval EFI_ABORTED             An error occurred.
+  @retval EFI_INVALID_PARAMETER   Invalid parameter.
+**/
+STATIC
+EFI_STATUS
+EFIAPI
+GicDIntcNodeParser (
+  IN  CONST FDT_HW_INFO_PARSER_HANDLE  FdtParserHandle,
+  IN  INT32                            GicIntcNode
+  )
+{
+  EFI_STATUS   Status;
+  INT32        AddressCells;
+  INT32        SizeCells;
+  CONST UINT8  *Data;
+  INT32        DataSize;
+  UINT32       RegSize;
+  UINT64       PhysicalBaseAddress;
+  UINT64       PhysicalBaseAddressRange;
+  VOID         *Fdt;
+  CONST UINT8  *GicDValue;
+  CONST UINT8  *GicDRange;
+
+  if (FdtParserHandle == NULL) {
+    ASSERT (FALSE);
+    return EFI_INVALID_PARAMETER;
+  }
+
+  Fdt = FdtParserHandle->Fdt;
+
+  Status = FdtGetParentAddressInfo (
+             Fdt,
+             GicIntcNode,
+             &AddressCells,
+             &SizeCells
+             );
+  if (EFI_ERROR (Status)) {
+    ASSERT (FALSE);
+    return Status;
+  }
+
+  // Don't support more than 64 bits and less than 32 bits addresses.
+  if ((AddressCells < 1) ||
+      (AddressCells > 2) ||
+      (SizeCells < 1) ||
+      (SizeCells > 2))
+  {
+    ASSERT (FALSE);
+    return EFI_ABORTED;
+  }
+
+  RegSize = (AddressCells + SizeCells) * sizeof (UINT32);
+
+  Data = FdtGetProp (Fdt, GicIntcNode, "reg", &DataSize);
+  if ((Data == NULL) ||
+      (DataSize < 0) ||
+      ((DataSize % RegSize) != 0))
+  {
+    // If error or wrong size.
+    ASSERT (FALSE);
+    return EFI_ABORTED;
+  }
+
+  GicDValue = Data + (sizeof (UINT32) *
+                      GET_DT_REG_ADDRESS_OFFSET (0, AddressCells, SizeCells));
+  GicDRange = Data + sizeof (UINT32) * AddressCells;
+
+  if (AddressCells == 2) {
+    PhysicalBaseAddress      = FdtReadUnaligned64 ((UINT64 *)GicDValue);
+    PhysicalBaseAddressRange = FdtReadUnaligned64 ((UINT64 *)GicDRange);
+  } else {
+    PhysicalBaseAddress      = FdtReadUnaligned32 ((UINT32 *)GicDValue);
+    PhysicalBaseAddressRange = FdtReadUnaligned32 ((UINT32 *)GicDRange);
+  }
+
+  if (FdtParserHandle->HwAddressInfo != NULL) {
+    Status = FdtParserHandle->HwAddressInfo (
+                                FdtParserHandle->Context,
+                                "GICD",
+                                PhysicalBaseAddress,
+                                PhysicalBaseAddressRange
+                                );
+    ASSERT_EFI_ERROR (Status);
+  }
+
+  return Status;
+}
+
+/** GICD information parser function.
+
+  @param [in]  FdtParserHandle  A handle to the parser instance.
+  @param [in]  FdtBranch        When searching for DT node name, restrict
+                                the search to this Device Tree branch.
+  @param [in]  GicVersion       The version of the GIC.
+
+  @retval EFI_SUCCESS             The function completed successfully.
+  @retval EFI_ABORTED             An error occurred.
+  @retval EFI_INVALID_PARAMETER   Invalid parameter.
+  @retval EFI_NOT_FOUND           Not found.
+  @retval EFI_UNSUPPORTED         Unsupported.
+**/
+EFI_STATUS
+EFIAPI
+ArmGicDInfoParser (
+  IN  CONST FDT_HW_INFO_PARSER_HANDLE  FdtParserHandle,
+  IN        INT32                      FdtBranch,
+  IN  CONST UINT32                     GicVersion
+  )
+{
+  EFI_STATUS  Status;
+  VOID        *Fdt;
+
+  if (FdtParserHandle == NULL) {
+    ASSERT (FALSE);
+    return EFI_INVALID_PARAMETER;
+  }
+
+  Fdt = FdtParserHandle->Fdt;
+
+  if (!FdtNodeHasProperty (Fdt, FdtBranch, "interrupt-controller")) {
+    ASSERT (FALSE);
+    return EFI_INVALID_PARAMETER;
+  }
+
+  // Parse the interrupt-controller depending on its Gic version.
+  switch (GicVersion) {
+    case 2:
+    case 3:
+    {
+      // Set the Gic version, then parse the GicD information.
+      Status = GicDIntcNodeParser (FdtParserHandle, FdtBranch);
+      break;
+    }
+    default:
+    {
+      // Unsupported Gic version.
+      ASSERT (FALSE);
+      return EFI_UNSUPPORTED;
+    }
+  }
+
+  return Status;
+}

OvmfPkg/Library/PlatformDeviceInfoLib/HwInfoParser/Arm/Gic/ArmGicDispatcher.c
@@ -1,0 +1,189 @@
+/** @file
+  Arm Gic dispatcher.
+
+  Copyright (c) 2021 - 2024, Arm Limited. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+  @par Reference(s):
+  - linux/Documentation/devicetree/bindings/interrupt-controller/arm,gic.yaml
+  - linux/Documentation/devicetree/bindings/interrupt-controller/arm,gic-v3.yaml
+**/
+
+#include "FdtUtility.h"
+#include "FdtInfoParser.h"
+#include "ArmGicCParser.h"
+#include "ArmGicDispatcher.h"
+#include "ArmGicDParser.h"
+#include "ArmGicRParser.h"
+
+/** List of "compatible" property values for GicV2 interrupt nodes.
+
+  Any other "compatible" value is not supported by this module.
+*/
+STATIC CONST COMPATIBILITY_STR  GicV2CompatibleStr[] = {
+  { "arm,cortex-a15-gic" }
+};
+
+/** COMPATIBILITY_INFO structure for the GICv2.
+*/
+CONST COMPATIBILITY_INFO  GicV2CompatibleInfo = {
+  ARRAY_SIZE (GicV2CompatibleStr),
+  GicV2CompatibleStr
+};
+
+/** List of "compatible" property values for GicV3 interrupt nodes.
+
+  Any other "compatible" value is not supported by this module.
+*/
+STATIC CONST COMPATIBILITY_STR  GicV3CompatibleStr[] = {
+  { "arm,gic-v3" }
+};
+
+/** COMPATIBILITY_INFO structure for the GICv3.
+*/
+CONST COMPATIBILITY_INFO  GicV3CompatibleInfo = {
+  ARRAY_SIZE (GicV3CompatibleStr),
+  GicV3CompatibleStr
+};
+
+/** Get the Gic version of an interrupt-controller node.
+
+  @param [in]  Fdt          Pointer to a Flattened Device Tree (Fdt).
+  @param [in]  IntcNode     Interrupt-controller node.
+  @param [out] GicVersion   If success, contains the Gic version of the
+                            interrupt-controller node.
+
+  @retval EFI_SUCCESS             The function completed successfully.
+  @retval EFI_INVALID_PARAMETER   Invalid parameter.
+  @retval EFI_UNSUPPORTED         Unsupported.
+**/
+STATIC
+EFI_STATUS
+EFIAPI
+GetGicVersion (
+  IN  CONST VOID    *Fdt,
+  IN        INT32   IntcNode,
+  OUT       UINT32  *GicVersion
+  )
+{
+  if ((Fdt == NULL) ||
+      (GicVersion == NULL))
+  {
+    ASSERT (FALSE);
+    return EFI_INVALID_PARAMETER;
+  }
+
+  if (FdtNodeIsCompatible (Fdt, IntcNode, &GicV2CompatibleInfo)) {
+    *GicVersion = 2;
+  } else if (FdtNodeIsCompatible (Fdt, IntcNode, &GicV3CompatibleInfo)) {
+    *GicVersion = 3;
+  } else {
+    // Unsupported Gic version.
+    ASSERT (FALSE);
+    return EFI_UNSUPPORTED;
+  }
+
+  return EFI_SUCCESS;
+}
+
+/** Gic dispatcher.
+
+  This disptacher parses the Device tree for the following infromation:
+   - GICC
+   - GICD
+   - GICR
+
+  @param [in]  FdtParserHandle A handle to the parser instance.
+  @param [in]  FdtBranch       When searching for DT node name, restrict
+                               the search to this Device Tree branch.
+
+  @retval EFI_SUCCESS             The function completed successfully.
+  @retval EFI_ABORTED             An error occurred.
+  @retval EFI_INVALID_PARAMETER   Invalid parameter.
+  @retval EFI_NOT_FOUND           Not found.
+  @retval EFI_UNSUPPORTED         Unsupported.
+**/
+EFI_STATUS
+EFIAPI
+ArmGicDispatcher (
+  IN  CONST FDT_HW_INFO_PARSER_HANDLE  FdtParserHandle,
+  IN        INT32                      FdtBranch
+  )
+{
+  EFI_STATUS  Status;
+  INT32       CpusNode;
+  INT32       IntcNode;
+  UINT32      GicVersion;
+  VOID        *Fdt;
+
+  if (FdtParserHandle == NULL) {
+    ASSERT (FALSE);
+    return EFI_INVALID_PARAMETER;
+  }
+
+  Fdt = FdtParserHandle->Fdt;
+
+  // The "cpus" node resides at the root of the DT. Fetch it.
+  CpusNode = FdtPathOffset (Fdt, "/cpus");
+  if (CpusNode < 0) {
+    return EFI_NOT_FOUND;
+  }
+
+  // Get the interrupt-controller node associated to the "cpus" node.
+  Status = FdtGetIntcParentNode (Fdt, CpusNode, &IntcNode);
+  if (EFI_ERROR (Status)) {
+    ASSERT (FALSE);
+    if (Status == EFI_NOT_FOUND) {
+      // Should have found the node.
+      Status = EFI_ABORTED;
+    }
+
+    return Status;
+  }
+
+  Status = GetGicVersion (Fdt, IntcNode, &GicVersion);
+  if (EFI_ERROR (Status)) {
+    ASSERT (FALSE);
+    return Status;
+  }
+
+  // Parse the GicC information.
+  Status = ArmGicCInfoParser (FdtParserHandle, IntcNode, GicVersion);
+  if (EFI_ERROR (Status)) {
+    // Don't try to parse GicD and GicMsiFrame information
+    // if no GicC information is found. Return.
+    ASSERT (Status == EFI_NOT_FOUND);
+    return Status;
+  }
+
+  // Parse the GicD information of the "cpus" interrupt-controller node.
+  Status = ArmGicDInfoParser (FdtParserHandle, IntcNode, GicVersion);
+  if (EFI_ERROR (Status)) {
+    // EFI_NOT_FOUND is not tolerated at this point.
+    ASSERT (FALSE);
+    return Status;
+  }
+
+  switch (GicVersion) {
+    case 4:
+    case 3:
+    {
+      // Parse the GicR information of the interrupt-controller node.
+      Status = ArmGicRInfoParser (FdtParserHandle, IntcNode, GicVersion);
+      if (EFI_ERROR (Status)) {
+        // EFI_NOT_FOUND is not tolerated at this point.
+        ASSERT (FALSE);
+        return Status;
+      }
+
+      break;
+    }
+    default:
+    {
+      ASSERT (FALSE);
+      return EFI_UNSUPPORTED;
+    }
+  }
+
+  return EFI_SUCCESS;
+}